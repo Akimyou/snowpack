a67c53356a92df005b76df28a329f69174303ddb
akimyou <akimyou@icloud.com>
2020-08-22 18:23:34 +0800

    add count.json

--- packages/@snowpack/app-template-blank/src/index.js
+++ packages/@snowpack/app-template-blank/src/index.js
@@ -1,8 +1,13 @@
 import { hello } from './hello.js';
 import { sayHello } from './sub.js';
+import count from './count.json'
 
 console.log('index say: ', hello);
 console.log('sub say:', sayHello());
+
+setInterval(() => {
+  console.log('count', count);
+}, 1000);
 
 if (import.meta.hot) {
   import.meta.hot.accept();
