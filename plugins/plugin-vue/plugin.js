e4a0674e5bd5d5878f3425f7ccd2ed353642731c
akimyou <akimyou@icloud.com>
2020-09-18 16:38:54 +0800

    fix plugin-vue for build bug

--- plugins/plugin-vue/plugin.js
+++ plugins/plugin-vue/plugin.js
@@ -96,7 +96,7 @@ module.exports = function plugin(snowpackConfig) {
           preprocessLang: descriptor.template.lang,
           compilerOptions: {
             scopeId: descriptor.styles.some((s) => s.scoped) ? `data-v-${id}` : null,
-            runtimeModuleName: '/web_modules/vue.js',
+            runtimeModuleName: 'vue',
           },
         });
         if (js.errors && js.errors.length > 0) {
